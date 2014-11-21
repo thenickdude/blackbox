9b50595d1d2eec14859c2f5fbd0c184278e1a1dd
Nicholas Sherlock <n.sherlock@gmail.com>
2014-11-21 14:05:46 +1300

    Improve broken frame debug printing Add --help mode

--- tools/blackbox/src/blackbox_decode.c
+++ tools/blackbox/src/blackbox_decode.c
@@ -12,41 +12,56 @@
 
 #include "parser.h"
 
-int optionRaw = 0, optionStatsLimits = 0, optionDebug;
-int optionLogNumber = -1;
-const char *optionFilename = 0;
+typedef struct decodeOptions_t {
+	int help, raw, limits, debug;
+	int logNumber;
+	const char *filename;
+} decodeOptions_t;
+
+decodeOptions_t options = {
+	.help = 0, .raw = 0, .limits = 0, .debug = 0,
+	.logNumber = -1,
+	.filename = 0
+};
 
 uint32_t lastFrameIndex = (uint32_t) -1;
 
-void onFrameReady(flightLog_t *log, int32_t *frame, int frameOffset, int frameSize)
+void onFrameReady(flightLog_t *log, bool frameValid, int32_t *frame, int frameOffset, int frameSize)
 {
 	int i;
 
-	if (optionDebug) {
-		lastFrameIndex++;
+	if (frame)
+		lastFrameIndex = (uint32_t) frame[FLIGHT_LOG_FIELD_INDEX_ITERATION];
 
-		while (lastFrameIndex != (uint32_t) frame[FLIGHT_LOG_FIELD_INDEX_ITERATION]) {
-			fprintf(stderr, "Missing frame %u\n", lastFrameIndex);
-			lastFrameIndex++;
+	if (frameValid) {
+		for (i = 0; i < log->fieldCount; i++) {
+			if (i == 0) {
+				printf("%u", (uint32_t) frame[i]);
+			} else {
+				if (log->fieldSigned[i] || options.raw)
+					printf(", %3d", frame[i]);
+				else
+					printf(", %3u", (uint32_t) frame[i]);
+			}
 		}
 
-		if (frame[FLIGHT_LOG_FIELD_INDEX_ITERATION] % 32 == 0)
-			printf("I ");
-		else
-			printf("P ");
+		if (options.debug) {
+			printf(", %c, offset %d, size %d\n", frame[FLIGHT_LOG_FIELD_INDEX_ITERATION] % 32 == 0 ? 'I' : 'P', frameOffset, frameSize);
+		} else
+			printf("\n");
+	} else if (options.debug) {
+		if (frame) {
+			/*
+			 * We'll assume that the frame's iteration count is still fairly sensible (if an earlier frame was corrupt,
+			 * the frame index will be smaller than it should be)
+			 */
+			fprintf(stderr, "Frame unusuable due to prior corruption %u, offset %d, size %d\n", lastFrameIndex, frameOffset, frameSize);
+		} else {
+			//We have no frame index for this frame, so just assume it was the one after the previously decoded frame
+			lastFrameIndex++;
+			fprintf(stderr, "Failed to decode frame %u, offset %d, size %d\n", lastFrameIndex, frameOffset, frameSize);
+		}
 	}
-
-	for (i = 0; i < log->fieldCount; i++) {
-		if (i > 0)
-			printf(", ");
-
-		printf("%3d", frame[i]);
-	}
-
-	if (optionDebug)
-		printf(", at %d, size %d", frameOffset, frameSize);
-
-	printf("\n");
 }
 
 void onMetadataReady(flightLog_t *log)
@@ -65,7 +80,7 @@ void onMetadataReady(flightLog_t *log)
 void printStats(flightLog_t *log, bool raw, bool limits)
 {
 	flightLogStatistics_t *stats = &log->stats;
-	uint32_t intervalMS = (stats->fieldMaximum[FLIGHT_LOG_FIELD_INDEX_TIME] - stats->fieldMinimum[FLIGHT_LOG_FIELD_INDEX_TIME]) / 1000;
+	int64_t intervalMS = (stats->fieldMaximum[FLIGHT_LOG_FIELD_INDEX_TIME] - stats->fieldMinimum[FLIGHT_LOG_FIELD_INDEX_TIME]) / 1000;
 
 	uint32_t goodBytes = stats->iFrameBytes + stats->pFrameBytes;
 	uint32_t goodFrames = stats->numIFrames + stats->numPFrames;
@@ -84,9 +99,6 @@ void printStats(flightLog_t *log, bool raw, bool limits)
 	else
 		fprintf(stderr, "Frames %8d\n", 0);
 
-	if (stats->numBrokenFrames)
-		fprintf(stderr, "%d frames failed to decode (%.2f%%)\n", stats->numBrokenFrames, (double) stats->numBrokenFrames / totalFrames * 100);
-
 	if (intervalMS > 0 && !raw) {
 		fprintf(stderr, "Data rate %4uHz %6u bytes/s %10u baud\n",
 				(unsigned int) (((int64_t) totalFrames * 1000) / intervalMS),
@@ -95,6 +107,11 @@ void printStats(flightLog_t *log, bool raw, bool limits)
 	} else {
 		fprintf(stderr, "Data rate: Unknown, no timing information available.\n");
 	}
+
+	if (totalFrames && (stats->numBrokenFrames || stats->numUnusableFrames))
+		fprintf(stderr, "\n%d frames failed to decode, rendering %d frames unusable in total (%ums, %.2f%%)\n",
+				stats->numBrokenFrames,
+				stats->numUnusableFrames, (unsigned int) (stats->numUnusableFrames * (intervalMS / totalFrames)), (double) stats->numUnusableFrames / totalFrames * 100);
 
 	if (limits) {
 		fprintf(stderr, "\n\n    Field name          Min          Max        Range\n");
@@ -119,13 +136,13 @@ int chooseLog(flightLog_t *log)
 	}
 
 	//Did the user pick a log to render?
-	if (optionLogNumber > 0) {
-		if (optionLogNumber > log->logCount) {
-			fprintf(stderr, "Couldn't load log #%d from this file, because there are only %d logs in total.\n", optionLogNumber, log->logCount);
+	if (options.logNumber > 0) {
+		if (options.logNumber > log->logCount) {
+			fprintf(stderr, "Couldn't load log #%d from this file, because there are only %d logs in total.\n", options.logNumber, log->logCount);
 			return -1;
 		}
 
-		return optionLogNumber - 1;
+		return options.logNumber - 1;
 	} else if (log->logCount == 1) {
 		// If there's only one log, just parse that
 		return 0;
@@ -141,6 +158,22 @@ int chooseLog(flightLog_t *log)
 	}
 }
 
+void printUsage(const char *argv0)
+{
+	fprintf(stderr,
+		"Blackbox flight log decoder by Nicholas Sherlock\n\n"
+		"Usage:\n"
+		"     %s [options] <logfilename.txt>\n\n"
+		"Options:\n"
+		"   --help         This page\n"
+		"   --index <num>  Choose the log from the file that should be decoded\n"
+		"   --limits       Print the limits and range of each field\n"
+		"   --debug        Show extra debugging information\n"
+		"   --raw          Don't apply predictions to fields (show raw field deltas)\n"
+		"\n", argv0
+	);
+}
+
 void parseCommandlineOptions(int argc, char **argv)
 {
 	int c;
@@ -148,9 +181,10 @@ void parseCommandlineOptions(int argc, char **argv)
 	while (1)
 	{
 		static struct option long_options[] = {
-			{"raw", no_argument, &optionRaw, 1},
-			{"debug", no_argument, &optionDebug, 1},
-			{"limits", no_argument, &optionStatsLimits, 1},
+			{"help", no_argument, &options.help, 1},
+			{"raw", no_argument, &options.raw, 1},
+			{"debug", no_argument, &options.debug, 1},
+			{"limits", no_argument, &options.limits, 1},
 			{"index", required_argument, 0, 'i'},
 			{0, 0, 0, 0}
 		};
@@ -164,13 +198,13 @@ void parseCommandlineOptions(int argc, char **argv)
 
 		switch (c) {
 			case 'i':
-				optionLogNumber = atoi(optarg);
+				options.logNumber = atoi(optarg);
 			break;
 		}
 	}
 
 	if (optind < argc)
-		optionFilename = argv[optind];
+		options.filename = argv[optind];
 }
 
 int main(int argc, char **argv)
@@ -181,14 +215,14 @@ int main(int argc, char **argv)
 
 	parseCommandlineOptions(argc, argv);
 
-	if (!optionFilename) {
-		fprintf(stderr, "Missing log filename argument\n");
+	if (options.help || !options.filename) {
+		printUsage(argv[0]);
 		return -1;
 	}
 
-    fd = open(optionFilename, O_RDONLY);
+    fd = open(options.filename, O_RDONLY);
     if (fd < 0) {
-    	fprintf(stderr, "Failed to open log file '%s': %s\n", optionFilename, strerror(errno));
+    	fprintf(stderr, "Failed to open log file '%s': %s\n", options.filename, strerror(errno));
     	return -1;
     }
 
@@ -199,8 +233,8 @@ int main(int argc, char **argv)
     if (logIndex == -1)
     	return -1;
 
-	if (flightLogParse(log, logIndex, onMetadataReady, onFrameReady, optionRaw)) {
-		printStats(log, optionRaw, optionStatsLimits);
+	if (flightLogParse(log, logIndex, onMetadataReady, onFrameReady, options.raw)) {
+		printStats(log, options.raw, options.limits);
 
 		// destroyFlightLog(log); Leaking it is faster.
 	} else {
